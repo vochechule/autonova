87271d5260bd621fff2d192d00fa2fc9ef4f8107
DanielNovak <ndan@post.cz>
2025-10-06 10:06:08 +0200

    hotfix

--- frontend/app/ads/[id]/page.tsx
+++ frontend/app/ads/[id]/page.tsx
@@ -3,10 +3,7 @@ import AdDetailServer from './AdDetailServer'
 // Export metadata generation from server component
 export { generateMetadata } from './AdDetailServer'
 
-interface AdDetailPageProps {
-  params: { id: string }
-}
 
-export default function AdDetailPage({ params }: AdDetailPageProps) {
+export default function AdDetailPage({ params }: { params: { id: string } }) {
   return <AdDetailServer params={params} />
 }
